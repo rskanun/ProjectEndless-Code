c5ef3d6ac378f60a9cb63be591821d9e6bbcb0b9
rskanun <55048282+rskanun@users.noreply.github.com>
2022-09-28 21:56:02 +0900

    9/28
    button

---
+++ endless project/Assets/Script/System/Menu/Button.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Script.System.Menu
+{
+    abstract public class Button : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject selectIcon;
+
+        [SerializeField]
+        private GameObject buttonIcon;
+
+        // 마우스 해당 버튼을 클릭했을 경우 작동되는 이벤트
+        public abstract void onButton();
+
+        private void OnMouseEnter()
+        {
+            selectIcon.transform.localPosition = buttonIcon.transform.localPosition;
+        }
+    }
+}
