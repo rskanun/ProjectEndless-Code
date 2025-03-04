cc613bc1a5ca9992b1f5bba79fe47112a4465a16
rskanun <bada6162@gmail.com>
2025-03-04 21:10:14 +0900

    Update Map System

--- Map/Area/AreaManager.cs
+++ Map/Area/AreaManager.cs
@@ -4,11 +4,12 @@ using UnityEngine.Tilemaps;
 [RequireComponent(typeof(PolygonCollider2D))]
 public class AreaManager : MonoBehaviour
 {
+    private static PolygonCollider2D lastEntedArea;
     [Header("구역 정보")]
     [SerializeField]
-    private BattleFieldData battleField;
-    [SerializeField]
     private PolygonCollider2D cameraArea;
+    [SerializeField]
+    private BattleFieldData battleField;
 
 #if UNITY_EDITOR
     private void OnValidate()
@@ -21,7 +22,42 @@ public class AreaManager : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
+            OnEnterArea();
+        }
+    }
+
+    private void OnEnterArea()
+    {
+        // 해당 구역이 처음이자 마지막인 경우
+        if (lastEntedArea == null)
+        {
+            // 현재 있는 구역으로 지정
             MapManager.SetCurrentArea(cameraArea);
         }
+
+        // 마지막 방문 구역으로 등록
+        lastEntedArea = cameraArea;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            OnExitArea();
+        }
+    }
+
+    private void OnExitArea()
+    {
+        // 현재 있는 구역에서 나가게 되면 
+        if (lastEntedArea == cameraArea)
+        {
+            // 나간 영역이 마지막 방문 구역일 경우 되돌리기
+            lastEntedArea = MapManager.CurrentArea;
+            return;
+        }
+
+        // 마지막으로 방문한 구역을 카메라 영역으로 변경
+        MapManager.SetCurrentArea(lastEntedArea);
     }
 }
---
+++ Map/Object/TranslucentObject.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(Tilemap))]
+public class TranslucentObject : MonoBehaviour
+{
+    private Tilemap tilemap;
+    private Coroutine colorChange;
+
+    // 알파값 변화
+    private float targetAlpha;
+    private float unit = 0.05f;
+    private float delay = 0.05f;
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        tilemap = GetComponent<Tilemap>();
+    }
+#endif
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            targetAlpha = 0.5f;
+
+            if (colorChange == null)
+            {
+                colorChange = StartCoroutine(ChangedAlpha());
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            targetAlpha = 1.0f;
+
+            if (colorChange == null)
+            {
+                colorChange = StartCoroutine(ChangedAlpha());
+            }
+        }
+    }
+
+    private IEnumerator ChangedAlpha()
+    {
+        while (tilemap.color.a != targetAlpha)
+        {
+            float a = tilemap.color.a;
+            if (a > targetAlpha) SetAlpha(a - unit);
+            else if (a < targetAlpha) SetAlpha(a + unit);
+
+            yield return new WaitForSeconds(delay);
+        }
+
+        colorChange = null;
+    }
+
+    private void SetAlpha(float a)
+    {
+        Color color = tilemap.color;
+        color.a = a;
+        tilemap.color = color;
+    }
+}
